7b4fb61aac43ce5fe6fbe9fe52b19484deb134c6
Durgansh Dixit <saniket919@gmail.com>
2022-09-14 17:26:47 +0530

    3 third commit to save work

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future <List<Data>> fetchData() async {
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+Future<List<Data>> fetchData() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
   if (response.statusCode == 200) {
     List jsonResponse = json.decode(response.body);
     return jsonResponse.map((data) => new Data.fromJson(data)).toList();
@@ -17,14 +19,19 @@ class Data {
   final int userId;
   final int id;
   final String title;
+  final bool completed;
+  //static const PrimaryColor =  Color(0xFF808080);
+   //bool abc=true;
 
-  Data({required this.userId , required this.id, required this.title});
+
+  Data({required this.userId, required this.id, required this.title , required this.completed});
 
   factory Data.fromJson(Map<String, dynamic> json) {
     return Data(
       userId: json['userId'],
       id: json['id'],
       title: json['title'],
+      completed:json['completed'],
     );
   }
 }
@@ -39,7 +46,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future <List<Data>> futureData;
+  late Future<List<Data>> futureData;
 
   @override
   void initState() {
@@ -56,27 +63,46 @@ class _MyAppState extends State<MyApp> {
           title: Text('Flutter GridView'),
         ),
         body: Center(
-          child: FutureBuilder <List<Data>>(
+          child:
+
+            FutureBuilder<List<Data>>(
             future: futureData,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 List<Data> data = snapshot.data ?? <Data>[];
-                return
-                  GridView.builder(
-                      itemCount: data.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        crossAxisSpacing: 5.0,
-                        mainAxisSpacing: 5.0,
-                      ),
-                      itemBuilder: (BuildContext context, int index) {
+                return GridView.builder(
+                    itemCount: data.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 5.0,
+                      mainAxisSpacing: 5.0,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      //if(data[index].completed) {
+                      if(true){
                         return Container(
                           height: 75,
-                          color: Colors.white,
-                          child: Center(child: Text(data[index].title),
-                          ),);
+                          color: Colors.green,
+                          child: Center(
+                            child: Text((data[index].title).toString()),
+                          ),
+
+
+                        );
                       }
-                  );
+                      else{
+                        return Container(
+                          height: 75,
+                          color: Colors.pink,
+                          child: Center(
+                            child: Text(data[index].title),
+                          ),
+
+
+                        );
+                      }
+
+                    });
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
